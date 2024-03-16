ef58ab61be8d432a35dfd5d1ce4b900c1dc0d206
BRIGHT <BRIGHT@DESKTOP-ST51CUU>
2024-03-16 15:19:19 +0300

    Add project files.

---
+++ MVC-ApiCall/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_ApiCall.Models;
+using System.Diagnostics;
+
+namespace MVC_ApiCall.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            // bir mvc uygulamasından api'ye istek atmak için RestCharp kullanılmaktadır.
+
+           // RestSharp.RestClient cli = new RestSharp.RestClient();
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
